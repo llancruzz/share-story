62b471af45e79e4e6e2091cd7223446d1648b178
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-01-11 14:45:20 +0000

    feat: Import summernote model to admin.py to organize painel admin

--- social_app/admin.py
+++ social_app/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
