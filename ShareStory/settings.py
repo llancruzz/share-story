a349cd3cbbaa18843f759d0553d0a358f69afdd4
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-01-21 16:35:55 +0000

    feat: Set DEV to True in env.py using os.environ

--- ShareStory/settings.py
+++ ShareStory/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
 
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'DEV' in os.environ
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
