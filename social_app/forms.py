2ca5610fc659b2a6da0d975c76f2f87ef2a913dc
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-26 10:26:35 +0000

    feat: Create profile edit form

--- social_app/forms.py
+++ social_app/forms.py
@@ -1,8 +1,29 @@
-from .models import Comment
+from .models import Comment, User
 from django import forms
+from django.contrib.auth.forms import UserChangeForm
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ("body",)
+
+
+class ProfileEditForm(UserChangeForm):
+    username = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    email = forms.CharField(max_length=100)
+    password = None
+
+    class Meta:
+        model = User
+        fields = ["username",  "first_name", "last_name", "email"]
+        widgets = {
+            "username": forms.TextInput(attrs={"class": "form-control"}),
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.TextInput(attrs={"class": "form-control"}),
+
+
+        }
