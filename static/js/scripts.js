e1a7e9e6262910afb65e55583240e7654ecf6e82
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-30 21:48:22 +0000

    feat: Update edit comment function

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,12 +3,13 @@
 function unhideCommentSection(event) {
   const editButton = event.target;
   const commentSection = editButton.previousElementSibling;
+  // const commentSection = document.getElementById("hide-text-area");
   commentSection.style.display = "block";
 }
 
 addEventListener("DOMContentLoaded", (event) => {
   const editCommentButtons = document.getElementsByClassName("edit-comment");
-  Array.prototype.forEach.call(editCommentButtons, function (element) {
+  Array.from(editCommentButtons).forEach((element) => {
     element.addEventListener("click", unhideCommentSection);
   });
 });
